e9d22aafd77fbdae8e232115384a118277be38f3
David Van <David@DavidVan.net>
2018-03-31 16:55:22 -0700

    Added modes and flipped state.

--- app.js
+++ app.js
@@ -40,15 +40,19 @@ app.listen(process.env.PORT || 3000, () => console.log('Server running on port 3
 app.post('/details', function(req, res) {
     let num_cards = 4;
     let details = {
-      Name: req.body.Name,
-      Phone: req.body.Phone,
-      Questions: {},
-      Answers: {},
-      Greeting: true
+        Name: req.body.Name,
+        Phone: req.body.Phone,
+        // Mode: req.body.Mode,
+        Mode: 'question', // placeholder. use above
+        // Flipped: req.body.Flipped,
+        Flipped: false, // placeholder. use above
+        Questions: {},
+        Answers: {},
+        Greeting: true
     };
     for (let i = 1; i <= num_cards; i++) {
-      details.Questions['Q' + i] = req.body['Q' + i];
-      details.Answers['A' + i] = req.body['A' + i];
+        details.Questions['Q' + i] = req.body['Q' + i];
+        details.Answers['A' + i] = req.body['A' + i];
     }
     app.set('details_' + details.Phone, details);
     client.calls.create({
@@ -68,36 +72,53 @@ app.post('/', (req, res) => {
     let twiml = new Twilio.twiml.VoiceResponse();
 
     if (!req.session.hasUser) {
-      user = { num_correct: 0 };
-      req.session.hasUser = 1;
+        user = {
+            num_correct: 0
+        };
+        req.session.hasUser = 1;
     }
     if (details.Greeting) {
-        twiml.say("Hello " + details.Name + ". Welcome to Quizlio!");
+        twiml.say({
+                voice: 'woman',
+            }, "Hello " + details.Name + ". Welcome to Quizlio!");
         details.Greeting = false;
     }
 
     const frontCard = Object.values(details.Questions);
     const backCard = Object.values(details.Answers);
 
-    Ask(frontCard, backCard, req.body.SpeechResult, twiml, res);
+    if (details.Mode === 'vocab') {
+        if (details.Flipped) {
+            Ask(backCard, frontCard, req.body.SpeechResult, twiml, res);
+        }
+        else {
+            Ask(frontCard, backCard, req.body.SpeechResult, twiml, res);
+        }
+    }
+    else { // otherwise, question mode
+        Ask(frontCard, backCard, req.body.SpeechResult, twiml, res);
+    }
 });
 
 app.post('/completed', (req, res) => {
     const details = app.get('details_' + req.body.Called);
 
-    const frontCard = Object.values(details.Questions);
-    const backCard = Object.values(details.Answers);
+    const frontCard = Object.values(details.Questions); // AKA Vocab Term
+    const backCard = Object.values(details.Answers); // AKA Definition
 
     let twiml = new Twilio.twiml.VoiceResponse();
     const answer = req.body.SpeechResult
-      .replace(/(\r\n\t|\n|\r\t)/gm, '')
-      .replace('.', '')
-      .toLowerCase()
-      .trim();
+        .replace(/(\r\n\t|\n|\r\t)/gm, '')
+        .replace('.', '')
+        .replace('?', '')
+        .toLowerCase()
+        .trim();
 
-    if (backCard[user.num_correct].toLowerCase() === answer) {
-        twiml.say('That is correct. The card back saids ' +
-            backCard[user.num_correct]);
+
+    if (details.Mode === 'question' && backCard[user.num_correct].toLowerCase() === answer) {
+        twiml.say({
+                voice: 'woman',
+            }, 'That is correct. The card back saids ' + backCard[user.num_correct]);
 
         twiml.redirect({
             method: 'POST'
@@ -107,6 +128,23 @@ app.post('/completed', (req, res) => {
 
         res.send(twiml.toString());
     }
+    else if (details.Mode === 'vocab') {
+        if (details.Flipped && frontCard[user.num_correct].toLowerCase().replace('.', '').replace('?', '').split(' ').includes(answer) || !details.Flipped && backCard[user.num_correct].toLowerCase() === answer) { // User guessing definition, so it should be contains/include OR User guessing word, so it should be an exact match
+            let vocabAnswer =  details.Flipped ? frontCard[user.num_correct] : backCard[user.num_correct];
+            twiml.say({
+                    voice: 'woman',
+                }, 'That is correct. The card says ' + vocabAnswer);
+
+            twiml.redirect({
+                method: 'POST'
+            }, '/');
+            user.num_correct++;
+            res.send(twiml.toString());
+        }
+        else {
+            incorrect(twiml, res);
+        }
+    }
     else {
         incorrect(twiml, res);
     }
@@ -114,27 +152,35 @@ app.post('/completed', (req, res) => {
 
 function Ask(frontCard, backCard, SpeechResult, twiml, res) {
     if (SpeechResult !== undefined && SpeechResult.replace('.', '').toLowerCase().trim() === 'yes') {
-        twiml.say('Skipping...');
+        twiml.say({
+                voice: 'woman',
+            }, 'Skipping...');
         user.num_correct += 1;
         twiml.redirect({
             method: 'POST'
         }, '/');
-    } else if (SpeechResult !== undefined && SpeechResult.replace('.', '').toLowerCase().trim() === 'no') {
-        twiml.say('Not skipping. Please try again.');
+    }
+    else if (SpeechResult !== undefined && SpeechResult.replace('.', '').toLowerCase().trim() === 'no') {
+        twiml.say({
+                voice: 'woman',
+            }, 'Not skipping. Please try again.');
         twiml.redirect({
             method: 'POST'
         }, '/');
-    } else {
-        twiml.say('Card ' + (user.num_correct + 1));
+    }
+    else {
+        twiml.say({
+                voice: 'woman',
+            }, 'Card ' + (user.num_correct + 1));
         twiml.gather({
-          input: 'speech',
-          timeout: 3,
-          action: '/completed'
-        })
-        .say({
-          voice: 'women',
-        },
-        frontCard[user.num_correct]);
+                input: 'speech',
+                timeout: 6,
+                action: '/completed',
+                voice: 'woman'
+            })
+            .say({
+                    voice: 'woman',
+                }, frontCard[user.num_correct]);
     }
 
     res.type('text/xml');
@@ -142,13 +188,18 @@ function Ask(frontCard, backCard, SpeechResult, twiml, res) {
 }
 
 function incorrect(twiml, res) {
-    twiml.say('That is incorrect.');
+    twiml.say({
+            voice: 'woman',
+        }, 'That is incorrect.');
     twiml.gather({
-        input: 'speech',
-        timeout: 3,
-        action: '/'
-    })
-    .say('Do you want to skip?');
+            input: 'speech',
+            timeout: 6,
+            action: '/',
+            voice: 'woman'
+        })
+        .say({
+            voice: 'woman',
+        }, 'Do you want to skip?');
 
     res.send(twiml.toString());
 }
