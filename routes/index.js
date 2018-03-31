7549a60b6a33045a27291412ffa6ef8ff23996ea
David Van <David@DavidVan.net>
2018-03-31 00:20:33 -0700

    Added reading of account details.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const router = express.Router();
+
+const accountSid = fs.readFileSync(path.join(__dirname, '../TwilioAccountSID'), 'utf8').replace(/\r?\n|\r/g,'');
+const authToken = fs.readFileSync(path.join(__dirname, '../TwilioAuthKey'), 'utf8').replace(/\r?\n|\r/g,'');
+
+const twilio = require('twilio');
+const client = new twilio(accountSid, authToken);
 
 // Handle GET request for index page.
 router.get('/', (req, res, next) => {
